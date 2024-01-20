230c9086bb988bcdadea575be2210724c8daf043
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2024-01-20 11:16:29 +0300

    EUAC2-20 : Added CompanyService

---
+++ src/main/java/com/cydeo/service/CompanyService.java
@@ -0,0 +1,21 @@
+package com.cydeo.service;
+
+ import com.cydeo.dto.CompanyDto;
+
+ import java.util.List;
+
+ public interface CompanyService {
+
+     CompanyDto findById(Long companyId);
+
+     List<CompanyDto> getCompanies();
+
+     CompanyDto createCompany(CompanyDto newCompany);
+
+     CompanyDto updateCompany(CompanyDto companyDto);
+
+     void activateCompany(Long companyId);
+
+     void deactivateCompany(Long companyId);
+
+ }
